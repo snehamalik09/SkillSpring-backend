19c6c52cd8820a378769c7f45255f217f2dadf1f
Sneha Malik <snehamalik962003@gmail.com>
2025-06-19 01:46:27 +0530

    rate limiting

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -8,8 +8,16 @@ import { logout } from "../controller/logout.js";
 import upload from "../../utils/multer.js";
 import User from "../models/User.model.js";
 import { SendEmail } from "../../utils/sendEmail.js";
+import {rateLimit} from 'express-rate-limit';
 
 const router = express.Router();
+const emailLimiter = rateLimit({
+  max:2,
+  windowMs:60*60*1000,
+  message: 'Message limit hit. Retry in 1 hour.',
+  standardHeaders: true,
+  legacyHeaders: false,
+})
 
 router.route('/register').post(register);
 router.route('/courses').get(getPublishedCourses);
@@ -18,7 +26,7 @@ router.route('/login').post(loginUser);
 router.route('/profile').get(isAuthenticated, getUserDetails);
 router.route('/updateProfile').patch(isAuthenticated, upload.single("profilePhoto"), updateProfile);
 router.route('/logout').get(logout);
-router.route('/contact').post(isAuthenticated, SendEmail);
+router.route('/contact').post(emailLimiter, SendEmail);
 
 
 router.get("/check-auth", isAuthenticated, async (req, res) => {
